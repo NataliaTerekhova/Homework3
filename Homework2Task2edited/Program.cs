6eb020bc14446ee1fad804c0e74179a8f2dce71e
NataliaTerekhova <121491406+NataliaTerekhova@users.noreply.github.com>
2023-01-24 14:50:40 +0300

    Added Homework 2 Task 2 edited

---
+++ Homework2Task2edited/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99)
+{ 
+   string str = number.ToString();
+   System.Console.WriteLine(str[2]);
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
