1ceada407a499d8919f43f59fc223310496edb12
Natalia <natalia_terekhova@yahoo.com>
2023-01-24 14:32:01 +0300

    Added Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int num = new Random().Next (1,21);
+System.Console.WriteLine( $"num={num}");
+
+for(int a=1; a <= num; a++)
+{
+    System.Console.WriteLine(Math.Pow(a, 3));
+}
